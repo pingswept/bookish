c2eb3236ed5e4a02027fb800a856bb4ff4418c68
pingswept <someone@somewhere.gov>
2017-02-25 15:58:13 -0500

    Start book lookup

--- bookish.py
+++ bookish.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
+import requests
 bookish = Flask(__name__, static_url_path='/static')
 
-@bookish.route("/")
-def test():
-    return "Hello World!"
+@bookish.route('/')
+def hello():
+    return render_template('index.html')
 
-@bookish.route('/hello/<name>')
-def hello(name=None):
-    return render_template('index.html', name=name)
+@bookish.route('/get-book-by-isbn', methods = ['GET', 'POST'])
+def get_book():
+    r = requests.get("https://openlibrary.org/api/books?bibkeys=ISBN:0451526538")
+    return r.text
 
 if __name__ == "__main__":
     bookish.run()
