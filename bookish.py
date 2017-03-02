f3a65f9199a828b6a0811722f2cfd76f991a1be5
pingswept <someone@somewhere.gov>
2017-03-02 15:34:18 -0500

    Remove extraneous columns

--- bookish.py
+++ bookish.py
@@ -24,7 +24,7 @@ def get_book():
     r = requests.get("https://openlibrary.org/api/books?bibkeys=ISBN:" + request.form['isbn'] + "&jscmd=data&format=json")
     book_data = json.loads(r.text)
     d = book_data['ISBN:' + request.form['isbn']]
-    data_list = ['', d['title'], d['by_statement'], '', '', d['identifiers']['isbn_13'][0][3:], d['identifiers']['isbn_13'][0],'','', d['publishers'][0]['name'], '', ''.join(n for n in d['pagination'] if n.isdigit()), d['publish_date'], '', '', 'owned', '', '\n']
+    data_list = [d['title'], d['by_statement'], d['identifiers']['isbn_13'][0][3:], d['identifiers']['isbn_13'][0], d['publishers'][0]['name'], '', ''.join(n for n in d['pagination'] if n.isdigit()), d['publish_date'], '', '', 'owned', '', '\n']
     csv_list = [commify(x) for x in data_list]
     result = ",".join(str(elem) for elem in csv_list)
     print result
